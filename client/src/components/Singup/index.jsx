a019dd83e2f47dbed8513499abbd5233e44758fa
remyhayaan <117528100+remyhayaan@users.noreply.github.com>
2024-12-04 16:58:07 +0300

    Update index.jsx

--- client/src/components/Singup/index.jsx
+++ client/src/components/Singup/index.jsx
@@ -21,7 +21,7 @@ const Signup = () => {
 		e.preventDefault();
 		try {
 			//const url = "http://localhost:8080/api/users";
-			const url = "http://localhost:8080/api/users";
+			const url = "https://server-km6l.onrender.com/api/users";
 			
 			const { data: res } = await axios.post(url, data);
 			navigate("/login");
